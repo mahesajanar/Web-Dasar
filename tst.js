b9800ef182f84897cc05c1319d3baf5d5b5bbaec
Mahesa Janar <104667065+mahesajanar@users.noreply.github.com>
2022-04-30 12:27:49 +0700

    Add files via upload

---
+++ tst.js
@@ -0,0 +1,17 @@
+let user = {
+    firstName: "Mahesa",
+    lastName: "Janar", 
+    age: 19, 
+    isMuggle: false,
+    stuff: ["Magic Wind", "Flying Car", "Owl"]
+};
+console.log("Hallo, nama saya " + user.firstName + " " + user.lastName);
+console.log("Umur saya " + user.age + " tahun" + "-dan ini teman-teman saya");
+
+
+
+let myArray = ["Miswar", "Irfan", "Ezra", "Teggar"];
+
+for(const arrayItem of myArray) {
+    console.log(arrayItem)
+}
